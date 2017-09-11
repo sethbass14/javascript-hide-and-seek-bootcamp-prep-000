1ff6f0348e64989f3a425dd1631fbd8bffdd1f0e
Seth Barden <sethbarden@gmail.com>
2017-09-11 15:29:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).toString()
+  }
+}
+
+function deepestChild() {
+  let current = document.getElementById('grand-node').querySelectorAll('div')
+  let next = []
+  while (current.length > 1) {
+      for (let i = 0; i < current.length; i++)  {
+        next.push(current[i].querySelectorAll('div'))// <div><div><div?>boo!<div><div><div>
+      }
+      current = next.shift()
+    }
+
+  return current[0]
+}
