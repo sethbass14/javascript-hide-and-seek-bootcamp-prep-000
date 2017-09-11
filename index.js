0e619f77dd0c8b47829e4f93b46d7290645d0d7b
Seth Barden <sethbarden@gmail.com>
2017-09-11 15:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let current = document.getElementById('grand-node')
+  let current = document.getElementById('grand-node').querySelectorAll('div')
   let next = []
   while (current.length > 1) {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i]) // <div>
+      current = current.querySelectorAll('div')// <div><div><div?>boo!<div><div><div>
       }
     current = next.shift()
   }
