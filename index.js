fccf45f391cd2c93fbfd5e44203b2fe952f0c0a5
Seth Barden <sethbarden@gmail.com>
2017-09-11 14:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
+
+function increaseRankyBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toSting()
+  }
+}
+
+
+function deepestChild() {
+  
+}
