f87b59b5d6e2e4418943fd23edf815695b2e40c0
Seth Barden <sethbarden@gmail.com>
2017-09-11 14:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
 
-function increaseRankBy(
-) {
+function increaseRankBy(n) {
   var lis = document.querySelectorAll('ul.ranked-list')
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + n).toString()
