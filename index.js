069beb6e772086c2b0a95d3d8ef020795a57e23e
Seth Barden <sethbarden@gmail.com>
2017-09-11 15:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deepestChild() {
   let next = []
   while (current.length > 1) {
       for (let i = 0; i < current.length, i++)  {
-        next.push(current[0].querySelectorAll('div')// <div><div><div?>boo!<div><div><div>
+        next.push(current[i].querySelectorAll('div')// <div><div><div?>boo!<div><div><div>
       }
       current = next.shift()
     }
